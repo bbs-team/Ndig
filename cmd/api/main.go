4ca36273c693198eec670706ec01ce02c9f5cdb9
seunghwan.na <seunghwan.na@navercorp.com>
2020-02-12 19:10:38 +0900

    add api, batch, cli

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "github.com/bbs-team/ndig/pkg/api"
+import (
+	"github.com/bbs-team/ndig/pkg/api"
+	"os"
+)
 
 func main()  {
-	s := api.NewServer("8080")
+	s := api.NewServer(os.Getenv("PORT"))
 	s.Start()
 }
